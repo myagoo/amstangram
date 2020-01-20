3903644c482aab1bad437a056afdf38a76f304dc
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-01-20 14:04:22 +0100

    Update Gallery animation

--- src/routes.js
+++ src/routes.js
@@ -9,28 +9,37 @@ import { Tangram } from "./pages/tangram"
 export const Routes = () => {
   return (
     <BrowserRouter>
-      <View>
+      <View
+        display="flex"
+        height="100vh"
+        width="100vw"
+        flexDirection="column"
+        overflow="hidden"
+      >
         <Header />
         <View
           display="flex"
           flexDirection="column"
-          height="100vh"
-          width="100vw"
+          height="100%"
+          position="relative"
         >
           <Switch>
             <Route exact path="/">
               <Tangram />
             </Route>
           </Switch>
-          <AnimatedRoute
+          <View
+            as={AnimatedRoute}
             path="/gallery"
             atEnter={{ offset: 100 }}
             atLeave={{ offset: 100 }}
             atActive={{ offset: 0 }}
             mapStyles={styles => ({
-              position: "absolute",
               width: "100%",
               height: "100%",
+              left: styles.offset,
+              position: "absolute",
+              flex: "1",
               transform: `translateX(${styles.offset}%)`,
             })}
             component={Gallery}
