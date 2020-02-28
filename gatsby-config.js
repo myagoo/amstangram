4881735cc167992b590276aef062964e6bf3f9d9
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-02-28 17:26:53 +0100

    Update gatsby-plugin-css-system

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,31 @@ module.exports = {
       },
     },
     "gatsby-transformer-svg",
-    "@css-system/gatsby-plugin-css-system",
+    {
+      resolve: "@css-system/gatsby-plugin-css-system",
+      options: {
+        theme: {
+          breakpoints: {
+            s: "40em",
+            m: "52em",
+            l: "64em",
+          },
+          space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+          fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+          colors: {
+            st1: "#FECA57",
+            st2: "#48DBFB",
+            mt1: "#1DD1A1",
+            lt1: "#FF6B6B",
+            lt2: "#8557E0",
+            sq: "#FF9FF3",
+            rh: "#54A0FF",
+            collision: "#bdc3c7",
+            background: "#b7efe0",
+          },
+        },
+      },
+    },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,6 @@
 const React = require("react")
 const { GalleryProvider } = require("./src/contexts/gallery")
-const { ThemeProvider } = require("./src/contexts/theme")
 
 exports.wrapPageElement = ({ element }) => {
-  return (
-    <ThemeProvider>
-      <GalleryProvider>{element}</GalleryProvider>
-    </ThemeProvider>
-  )
+  return <GalleryProvider>{element}</GalleryProvider>
 }
