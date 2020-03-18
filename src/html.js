54c1df15b80096212fc59a8fdec776c2c7a36332
Benjamin Millagou <benjamin@jfg-networks.net>
2020-03-18 22:11:33 +0100

    add html title

--- src/html.js
+++ src/html.js
@@ -4,6 +4,7 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <title>Amstangram</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
