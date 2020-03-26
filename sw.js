067542535b0ec32e8c38c2863ea66e018b5a4d2f
Benjamin Millagou <benjamin@jfg-networks.net>
2020-03-26 22:10:13 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b963e54cfd06593290f8.js"
+    "url": "webpack-runtime-179422816219b1e8a9c8.js"
   },
   {
     "url": "commons-f9f1ac872bd74792144d.js"
   },
   {
-    "url": "app-3a13c75f9fab4ee6eb96.js"
+    "url": "app-f6a49adfd170a987b2d2.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-f3f35f6ef3789022665d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3c00ae987f2a342f1aecb2d9458909e7"
+    "revision": "ea6b76668369929efb7fa70b4242fbe6"
   },
   {
     "url": "clipper_unminified.js"
@@ -140,7 +140,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-3a13c75f9fab4ee6eb96.js`))) {
+  if (!resources || !(await caches.match(`/app-f6a49adfd170a987b2d2.js`))) {
     return await fetch(event.request)
   }
 
