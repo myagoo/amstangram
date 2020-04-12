786ba0aa500592b64e9e513b3f7aa6305a475cb4
Benjamin Millagou <benjamin@jfg-networks.net>
2020-04-12 22:23:26 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0a02cf9d81eb0980837e.js"
+    "url": "webpack-runtime-969ecce17353b026a190.js"
   },
   {
     "url": "commons-f9f1ac872bd74792144d.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "03de9a24acc079f3cc9339b88f96da19"
+    "revision": "1fc4394d91aee8775527efccfea6f091"
   },
   {
     "url": "clipper_unminified.js"
