dec2923fe8eed7c776cfe0c2d3d924d549211cdc
Benjamin Millagou <benjamin@jfg-networks.net>
2020-04-22 23:21:39 +0200

    remove unused icons

--- src/contexts/showBackgroundPattern.js
+++ src/contexts/showBackgroundPattern.js
@@ -7,41 +7,6 @@ import React, {
   useContext,
 } from "react"
 
-export const DashedTangramIcon = (props) => {
-  return (
-    <svg
-      viewBox="-30 -30 413.55 413.55"
-      stroke="currentColor"
-      strokeWidth="30"
-      strokeLinejoin="round"
-      strokeDasharray="60"
-      fill="transparent"
-      width="32"
-      height="32"
-      {...props}
-    >
-      <path d="M353.55339,302.84271l-70.71068,70.71068l-212.13203,0l-70.71068,-70.71068zM184.4269,282.84271v-141.42136l141.42136,-141.42136l0,141.42136z"></path>
-    </svg>
-  )
-}
-
-export const FilledTangramIcon = (props) => {
-  return (
-    <svg
-      viewBox="-30 -30 413.55 413.55"
-      stroke="currentColor"
-      strokeWidth="30"
-      strokeLinejoin="round"
-      fill="currentColor"
-      width="32"
-      height="32"
-      {...props}
-    >
-      <path d="M353.55339,302.84271l-70.71068,70.71068l-212.13203,0l-70.71068,-70.71068zM184.4269,282.84271v-141.42136l141.42136,-141.42136l0,141.42136z"></path>
-    </svg>
-  )
-}
-
 export const ShowBackgroundPatternContext = createContext([true, () => {}])
 
 export const useShowBackgroundPattern = () =>
