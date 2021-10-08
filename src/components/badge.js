3d72905112f4a0968833b20d96db074f664d2d79
Benjamin Millagou myagoo <benjamin@jfg-networks.net>
2021-10-08 11:08:25 +0200

    Fix avatar generation

--- src/components/badge.js
+++ src/components/badge.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useMemo } from "react"
 import { View } from "./view"
 import { SoundContext } from "../contexts/sound"
 import { ThemeContext } from "css-system"
@@ -6,11 +6,16 @@ import { ThemeContext } from "css-system"
 export const Badge = ({ uid, size = "badge", css, onClick, ...props }) => {
   const { playButton } = useContext(SoundContext)
   const theme = useContext(ThemeContext)
-  const urlSize = Math.max(40, theme.sizes[size] || size)
+
+  const backgroundColor = useMemo(() => {
+    const pieceColors = Object.values(theme.colors.pieces)
+    return pieceColors[uid.charCodeAt(0) % pieceColors.length]
+  }, [theme, uid])
+
   return (
     <View
       as="img"
-      src={`https://api.adorable.io/avatars/${urlSize}/${uid}.png`}
+      src={`https://avatars.dicebear.com/4.9/api/croodles-neutral/${uid}.svg?size=${theme.sizes[size]}&b=${encodeURIComponent(backgroundColor)}`}
       css={{
         m: "2px",
         boxShadow: `0 0 0 2px ${theme.colors.dialogText}`,
